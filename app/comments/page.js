b56fe1824b26401425d6359759ca8701ebf38f9f
ThNs7 <thiagoo.nasci77@gmail.com>
2025-09-16 19:26:00 -0300

    fix: CommentsPage

--- app/comments/page.js
+++ app/comments/page.js
@@ -1,7 +1,12 @@
 "use client";
 
+import { Suspense } from "react";
 import CommentsPage from "../components/CommentsPage";
 
-export default function CommentsRoute() {
-  return <CommentsPage />;
+export default function Page() {
+  return (
+    <Suspense fallback={<div>Carregando comentários...</div>}>
+      <CommentsPage />
+    </Suspense>
+  );
 }
